ac16e8cb4c26187e1b4a43e189c9523e92a04d6a
singhakash006 <114005250+singhakash006@users.noreply.github.com>
2022-12-20 18:35:12 +0530

    Create BinarySearchleetcode.cpp

---
+++ BinarySearchleetcode.cpp
@@ -0,0 +1,15 @@
+int search(int* nums, int numsSize, int target){
+int low=0, high=numsSize-1,mid;
+while(low<=high){
+     mid=low+(high-low)/2;
+    if(target==nums[mid]){
+        return mid;
+    }
+    else if(nums[mid]<target){
+        low=mid+1;}
+    else {
+        high=mid-1;
+    }
+  
+}
+return -1;}
